c7d394ed8210b2ab0f168e59a662d15c2179a794
Akshat Tiwari <akshattiwari0901@gmail.com>
2024-03-06 23:05:53 +0530

    Added dual color theme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
       <App />
+      <SpeedInsights />
     </ChakraProvider>
   </React.StrictMode>
 );
